b933777f228fc39c8e5fc73a3ad1321df1e5b8fe
heatterr-a11y <heat.terr@icloud.com>
2026-02-05 06:23:33 -0600

    Refactor service worker for cleaner syntax

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-selfconst CACHE_NAME = "ontime-marketing-v1";
+const CACHE_NAME = "ontime-marketing-v1";
 const ASSETS = [
   "/",
   "/index.html",
@@ -8,21 +8,15 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+  ));
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
